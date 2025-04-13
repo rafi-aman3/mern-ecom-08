a9414060f0cd3a1d85c1f0b53ae81c267d913c70
rafi-aman3 <rafiaman03@gmail.com>
2025-04-13 19:05:01 +0600

    last class update

---
+++ src/routes.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+
+const AppRoutes = () => {
+	return (
+		<Routes>
+			<Route path='/' element={<HomePage />} />
+		</Routes>
+	);
+};
+
+export default AppRoutes;
